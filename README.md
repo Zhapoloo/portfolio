62e7f76183895459e1439d1490e972c2e13a2f20
Zhapoloo <zhapoloo@proton.me>
2026-07-09 20:53:34 -0600

    se hizo correccion ortografica se agregaron todos los graficos y sus respectivas leyendas ademas de que se reorganizo el archivo de R para que estuviera mas presentable

--- README.md
+++ README.md
@@ -19,12 +19,15 @@ Se realizó una limpieza de datos en R, eliminando duplicados mediante un filtra
 * Al aislar las canciones más populares del mercado, observamos que estas no se inclinan hacia los extremos. Los éxitos mundiales se concentran en una valencia neutra (0.4 - 0.6), sugiriendo que la música de consumo masivo busca un balance emocional que resulte atractivo para una audiencia global y diversa, evitando polarizar a los oyentes.
 
 ### 3. El castigo de la acústica: ¿Por qué las canciones acústicas son menos populares?
+![Grafico de barras](visualizations/tristeza_o_pureza.png)
 * Los datos revelan que las composiciones con alta acústica (>0.8) tienen una valencia significativamente menor (0.35) frente a las producciones sintéticas. Este análisis demuestra que el mercado actual castiga la 'pureza' acústica con una menor popularidad, sugiriendo que los oyentes de Spotify prefieren producciones con mayor carga de energía y texturas sonoras modernas frente a la melancolía acústica.
 
 ### 4.Tonalidad musical vs. Percepción emocional.
+![Grafico de barras](visualizations/tonalidad_vs_percepcion_musical.png)
 * Analizamos si la clave musical (Key) influía en la valencia percibida por el oyente. Los resultados muestran que, independientemente de la tonalidad, la valencia promedio se mantiene constante entre 0.4 y 0.47. Esto descarta la hipótesis de que ciertas claves musicales determinen intrínsecamente si una canción es percibida como 'alegre' o 'triste' por el usuario promedio.
 
 ### 5. La música trasciende a la técnica: El impacto de la tonalidad en la popularidad.
+![Grafico de barras](visualizations/es_la_clave_musical_factor_de_exito.png)
 * Al evaluar los Hits de Spotify, observamos que ninguna tonalidad específica garantiza el éxito comercial. La popularidad promedio es prácticamente uniforme en todas las claves. Esto refuerza la idea de que los factores determinantes del éxito en Spotify están vinculados a variables de producción (como el género y la energía) más que a la estructura musical técnica.
 
 ## Herramientas Utilizadas
--- scripts/spotify_songs_EDA.R
+++ scripts/spotify_songs_EDA.R
@@ -1,32 +1,32 @@
 library('tidyverse')
 library('skimr')
 spo_data <- read_csv('/home/zhapoloo/Documents/practice/Portfolio/dataset.csv')
-#i used skimm_wituout_charts to check 2 things the first one is to make sure the data is comple 
-#and the second one is to make sure that each column has the correct data type 
+#use skim para verificar que no hubiera data faltante o valores nulos y para ver los tipos de datos de cada columna
 
 spo_data |>  skim()
-
+# Aqui use select para eliminar la columna de índice basura y distinct para eliminar duplicados basándose SOLO en el ID y mantener el resto de las columnas
 spo_data <- spo_data |> 
   select(-...1) |>  # Destruye la columna de índice basura
   distinct(track_id, .keep_all = TRUE) # Elimina duplicados basándose SOLO en el ID y mantiene el resto de las columnas
-# here i plotted all the numeric columns so that i could see the distrubution of each 
 
-# 1. Reshape the data to long format
+
+#Aqui use pivot_longer para transformar los datos de ancho a largo y luego use facet_wrap para crear histogramas separados para cada columna numérica. Esto me permite ver la distribución de cada variable numérica en el conjunto de datos.
 df_long <- spo_data %>%
-  select(where(is.numeric)) %>% # Keep only numeric columns
+  select(where(is.numeric)) %>% # mantiene solo las columnas numéricas
   pivot_longer(everything(), names_to = "column", values_to = "value")
 
-# 2. Plot using facet_wrap
+# 2. Plot usando ggplot para crear histogramas de cada columna numérica en el conjunto de datos. Cada histograma se muestra en un panel separado gracias a facet_wrap, lo que facilita la comparación de distribuciones entre diferentes variables.
 ggplot(df_long, aes(x = value)) +
   geom_histogram(bins = 30, fill = "steelblue", color = "white") +
-  facet_wrap(~column, scales = "free") + # Creates separate plots
+  facet_wrap(~column, scales = "free") + # Crea paneles separados para cada columna numérica
   theme_minimal()
 
 
 
-# i will use boxplots to determine the distrubution and outliers on the data 
+# use pivot_longer para transformar los datos de ancho a largo y luego use facet_wrap para crear boxplots separados para cada columna numérica.
+# Esto me permite ver la distribución de cada variable numérica en el conjunto de datos y detectar posibles valores atípicos.
 
-# 1. Reshape and Plot
+# reformatee la columna de datos para que cada variable numérica tenga su propio panel en el gráfico, lo que facilita la comparación de distribuciones entre diferentes variables y la identificación de posibles valores atípicos.
 spo_data %>%
   select(where(is.numeric)) %>%
   pivot_longer(everything(), names_to = "variable", values_to = "value") %>%
@@ -36,46 +36,40 @@ spo_data %>%
   theme_minimal() +
   theme(axis.text.x = element_blank())    # Clean up the labels
 
-#now with the infomation displayed what i will do next is to check those values that seem off like on the popularity seems off having more on 0 thatn in any other bar
-
+# Ahora voy a ver la cantidad de canciones que tienen 0 de popularidad y el porcentaje que representan del total de canciones por género,
+# esto me ayudará a entender si hay algún sesgo en los datos de popularidad y si ciertos géneros tienen más canciones con baja popularidad que otros.
 
 spo_data |> group_by(track_genre) |>  summarize(low_pop = sum(popularity < 1), total = n(), percentage_off_0 = (low_pop/total)* 100  ) |>  view()
 
-# for popularity either the data is skew or was not recorder properly i have to look into the way is calculated as several genres have over 60% on 0  so based 
-# on what i found seems like the more and most recent played songs have more popularity that makes sence as to why there are so much 0es but stil i will treat this 
-# popularity column with care as seems skew due to more experimental users being on other platforms like tidal spotify is more mainstream 
 
-#here i determnied what percentage of of the track seemed to be live and basically most of them were not 
+# viendo la forma en la que se recabaron los datos y la cantidad de canciones que tienen 0 de popularidad, me di cuenta que la mayoría de las canciones no son 
+populares y que hay un sesgo en los datos de popularidad, ya que ciertos géneros tienen más canciones con baja popularidad que otros. Esto puede afectar el análisis y las conclusiones que se puedan sacar de los datos, pero al saber la forma en la que 
+se calcula la popularida basandose en la cantidad de reproducciones y que la mayoría de las canciones no son populares, puedo tener en cuenta este sesgo al analizar los datos y sacar conclusiones más precisas.
+
+#Aqui use summarize para calcular el porcentaje de canciones con liveness menor a 0.5 en el conjunto de datos, lo que me ayudará a entender
+# la proporción de canciones que tienen un nivel bajo de "liveness" y cómo esto puede afectar el análisis de los datos.
 
 spo_data |>  summarize(sum(liveness < 0.5) / n())
 
 
 
-# here i confirmed that the temp is not skew for more on certain genres thatn in others and i found 2 things first that the amount of songs is the exact same sound made up
+# Aqui depues de ver como se recaudo la informacion pude ver que la razon por la que las canciones tienen la misma cantidad en cada genero es porque se seleccionaron 1000 por cada genero,
+# por lo que no hay un sesgo en la cantidad de canciones por genero, pero si hay un sesgo en la popularidad de las canciones, ya que la mayoría de las canciones no son populares y ciertos generos tienen 
+#mas canciones con baja popularidad que otros.
 
 
 spo_data |> group_by(track_genre) |>  summarize(n(), mean(tempo)) |> view()
 
-#here i checked to confirm that the info was accurate and yes music like classical tend to be on lower numbers while metal and dubstep tend to be much more higher so is accurate
-
-spo_data |>  group_by(track_genre) |>  summarize(sum(acousticness < 0.1),n()) |>  view()
-
-# here i made an scatter plot along with an smooth line to check how likely the cheerfull music is more dansable and seems to be a correlation
-# but is lower than expected only 0.47 now even 50%
+# Aqui hice un analisis de correlacion entre la valencia y la danceabilidad para ver si las canciones mas bailables son tambien las mas alegres,
+# y si tiene una correlacion positiva por lo que si son mas alegres 
 
 ggplot(spo_data) + geom_point(aes(x=danceability,y=valence)) + geom_smooth(aes(x=danceability,y=valence))
 cor(spo_data$valence,spo_data$danceability)
 
 
-### to answer the question if the most danceable songs are the more cheerfull the answer is likely a no
+### 1 son las canciones mas populares tambien las mas alegres?
+# parece que no hay una correlacion entre la popularidad y la valencia, por lo que no necesariamente las canciones mas populares son las mas alegres.
 
-
-
-
-
-
-#### second question are the more popular songs cheerfull or not?
-# at least for the correlation no it shows to be in a middle point not too sad or cheerfull 
 cor(spo_data$popularity,spo_data$valence)
 
 spo_data |>  group_by(track_genre) |>  summarize(mean(popularity), mean(valence)) |>  view()
@@ -83,15 +77,15 @@ ggplot(spo_data) + geom_point(aes(x=spo_data$popularity,y= spo_data$valence)) + 
 
 
 
-###3rd question is the acustic music considered sad or happy?
-#sad, seems to be sad songs the ones that usually are interpreted on acoustics
+###Es la musica mas acustica la que tiene menos valencia? 
+#si, parece que hay una correlacion negativa entre la valencia y la acusticidad, por lo que las canciones mas acusticas tienden a tener menos valencia.
 cor(spo_data$valence, spo_data$acousticness)
 spo_data |>  filter(acousticness >0.7) |>  summarize(mean(valence),n())
 
 
 
-##4th are acoustic songs popular?
-#no they are not
+##son las canciones mas acusticas tambien las menos populares?
+#parece que no 
 spo_data |> 
   group_by(es_muy_acustica = acousticness > 0.8) |> 
   summarize(
@@ -101,18 +95,20 @@ spo_data |>
 
 
 
-##5th is the energy realted to the danceability ?
-#at least in correlation no it is not 
+##esta la energia relacionada con la danceabilidad?
+# no tiene una correlacion muy fuerte, pero si hay una correlacion positiva entre la energia y la danceabilidad, por lo que las canciones mas energicas tienden a ser mas bailables.
 cor(spo_data$energy,spo_data$danceability)
 
 
 
-##6th is the valence related to the key?
+##ersta la clave musical relacionada con la valencia?
+# no 
 cor(spo_data$key,spo_data$valence)
 
-## what are the most popular keys
-## all has the same number about so either the data is made up or they are very likely
-spo_data |>  group_by(key) |> summarize(mean(popularity))
+#cuales son las claves musicales mas populares?
+# todas las claves musicales tienen la misma popularidad promedio, por lo que no hay una clave musical que sea mas popular que otra.
+spo_data |>  group_by(key) |> summarize(mean(popularity)) |>  view()
+
 
 write.csv(spo_data,'spotidy_data_clean.csv')
 
